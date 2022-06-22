6d7e44219feb43f14e48ccfdb62acb0eba33e639
Dmitry <22072003dz2@gmail.com>
2022-06-22 16:16:02 +0300

    Minified files

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,25 +9,29 @@ import toTop from "./modules/toTop.js";
 import adaptiveAnimate from "./modules/adaptiveAnimate.js";
 import mobileNav from "./modules/mobileNav.js";
 
-// scroll animations for animate.css
-new WOW().init()
-// scroll animations for elements with ".js-scroll"
-animateWithScroll()
-// tabs
-tabs('.route-info__title-item', '.route-info__content-item')
-// gallery 
-gallery('.gallery-watch')
-// phone mask
-mask('.route-download__input')
-// download pdf file second section
-downloadFile('.route-download__btn', 'download-files/1.pdf', '.route-download__input')
-// liner video popup
-videoPopup('https://www.youtube.com/embed/85a0e62bP2E', '.liner-video__btn')
-// modal window
-modal('[data-open-modal]')
-// to top arrow
-toTop()
+document.addEventListener('DOMContentLoaded', () => {
+  // scroll animations for animate.css
+  new WOW().init()
+  // scroll animations for elements with ".js-scroll"
+  animateWithScroll()
+  // tabs
+  tabs('.route-info__title-item', '.route-info__content-item')
+  // gallery 
+  gallery('.gallery-watch')
+  // phone mask
+  mask('.route-download__input')
+  // download pdf file second section
+  downloadFile('.route-download__btn', 'download-files/1.pdf', '.route-download__input')
+  // liner video popup
+  videoPopup('https://www.youtube.com/embed/85a0e62bP2E', '.liner-video__btn')
+  // modal window
+  modal('[data-open-modal]')
+  // to top arrow
+  toTop()
 
-adaptiveAnimate()
-mobileNav()
+  adaptiveAnimate()
+  mobileNav()
+})
+
+
 
